53baffc81ff32d79867773e870069170d5bfe536
NICOLO' IACOBONE s317025 <nicolo.iacobone@studenti.polito.it>
2025-08-25 18:45:22 +0200

    migliorato output slurm

--- slurm/slurm_spatrackerv2_scratch.sh
+++ slurm/slurm_spatrackerv2_scratch.sh
@@ -49,6 +49,7 @@ for video in /cluster/work/igp_psr/niacobone/examples/edge_case/*.mp4; do
     video_name=$(basename "$video" .mp4)
     echo "Processing video: $video_name"
     python inference.py --video_name="$video_name"
+    echo "----------------------------------"
 done
 
 echo "=== Job finished at $(date) ==="
