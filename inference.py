f00df2d7683814bfb2703cf6334b813a50e02900
NICOLO' IACOBONE s317025 <nicolo.iacobone@studenti.polito.it>
2025-09-10 18:13:12 +0200

    debug

--- inference.py
+++ inference.py
@@ -135,6 +135,7 @@ if __name__ == "__main__":
                 unc_metric = None
             elif args.data_type == "RGB":
                 # Se args.data_dir è una cartella, carica immagini; se è un file, carica video
+                print(vid_path)
                 if os.path.isdir(vid_path):
                     frames_path = os.path.join(vid_path, "frames")
                     video_tensor = load_frames(frames_path)
